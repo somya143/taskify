e8211f3cdff73f822d3bf43d46e5dafe781f29db
somya ranjan singh <maaheersj@gmail.com>
2022-11-06 20:48:52 +0530

    taskify

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-
+import {AiFillEdit, AiFillDelete} from "react-icons/ai";
+import {MdDoneAll} from "react-icons/md";
+import "./styles.css";
+import TodoList from './TodoList';
 
 type TodoProps = {
     id : number;
@@ -12,9 +15,44 @@ type TodoProps = {
     setTodos : React.Dispatch<React.SetStateAction<TodoProps[]>>; 
   }
 const SingleTodo = ({title,todos,setTodos}: Props) => {
+
+const handleDone = (id:number) => {
+setTodos(
+    todos.map((el) => 
+    el.id === id ? {...title , isDone: !title.isDone} : title
+    )
+)
+}
+
+const handleDelete = (id:number) => {
+    setTodos(
+        todos.filter((el) => el.id !== id)
+    )
+}
+
   return (
-    <form action="">
-        <span className="todos single__text">{title.title}</span>
+    <form action="" className='todo__single'>
+
+        {
+            title.isDone ? (
+                <s className="todos_single__text">{title.title}</s>
+            ): (
+                <span className="todos_single__text">{title.title}</span>
+            )
+        }
+      
+
+        <div>
+            <span className="icon">
+            <AiFillEdit />
+            </span>
+            <span className="icon"  onClick={() => handleDelete(title.id)}>
+                <AiFillDelete />
+            </span>
+            <span className="icon" onClick={() => handleDone(title.id)} >
+                <MdDoneAll />
+            </span>
+        </div>
     </form>
   )
 }
